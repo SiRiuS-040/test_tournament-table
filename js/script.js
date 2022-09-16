8ca68d5600e7e39fdcc60267b7d5ab9341b353b3
SiRiuS-040 <79222154+SiRiuS-040@users.noreply.github.com>
2022-09-16 09:11:32 +0300

    логика сетки

--- js/script.js
+++ js/script.js
@@ -21,6 +21,11 @@ document.addEventListener('DOMContentLoaded', function (evt) {
             } else {
                 team.classList.add('ready')
             }
+
+            // 
+
+            team.classList.add('battle')
+
         });
 
         let newTeamArr = [...teamsArr]
@@ -45,21 +50,31 @@ document.addEventListener('click', function (evt) {
         let parent = target.closest('.tournament__matchup');
         let teamArr = parent.querySelectorAll('.tournament__team')
 
+        let allMatchArr = document.querySelectorAll('.tournament__matchup')
 
         function getScore(parent) {
-
             teamArr.forEach(team => {
                 let teamScore = team.querySelector('.tournament__team-score')
-                teamScore.textContent = getRandomInt(0, 100)
+                teamScore.textContent = getRandomInt(0, 10)
             });
-            target.classList.add('hidden')
+            let firstTeamScore = teamArr[0].querySelector('.tournament__team-score').textContent;
+            let SecondTeamScore = teamArr[1].querySelector('.tournament__team-score').textContent;
+            if (firstTeamScore == SecondTeamScore) {
+                console.log('счет одинаковый');
 
-            // teamArr.forEach(team => {
-            //     let teamScore = team.querySelector('.tournament__team-score')
-            //     if () {
-
-            //     }
-            // });
+            } else if (firstTeamScore > SecondTeamScore) {
+                teamArr[0].classList.add('winner')
+                teamArr[1].classList.remove('ready')
+                teamArr[0].classList.remove('ready')
+                teamArr[1].classList.add('looser')
+                target.classList.add('hidden')
+            } else {
+                teamArr[1].classList.add('winner')
+                teamArr[1].classList.remove('ready')
+                teamArr[0].classList.remove('ready')
+                teamArr[0].classList.add('looser')
+                target.classList.add('hidden')
+            }
 
 
         }
@@ -70,23 +85,83 @@ document.addEventListener('click', function (evt) {
             return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
         }
 
-        // teamArr.forEach(team => {
-        //     if (!team.classList.contains('ready')) {
-        //         console.log('нет команд');
-        //     }
-        // });
+        function mooveWinner() {
+            let currentMatch = parent.getAttribute('data-match')
+            let currentRound = parent.getAttribute('data-round')
+            teamArr.forEach(team => {
+                // let teamNumber = team.getAttribute('data-team')
+                if (team.classList.contains('winner')) {
+                    let teamName = team.querySelector('.tournament__team-name').textContent;
+                    // console.log(team);
+                    // console.log(currentRound);
+                    console.log(currentMatch);
+                    console.log(Number(currentRound));
+                    let findTeamArr = document.querySelectorAll('.tournament__team');
+                    findTeamArr.forEach(findTeemm => {
+                        let findTeamID = findTeemm.getAttribute('data-team');
+                        let findMatch = findTeemm.closest('.tournament__matchup');
+                        let findMatchRound = findMatch.getAttribute('data-round');
+                        let findTeamName = findTeemm.querySelector('.tournament__team-name');
+                        let setTeamName = team.querySelector('.tournament__team-name').textContent;
+                        // let findMatchRound = 
+                        // console.log(currentMatch);
+                        // console.log(Number(currentRound));
+                        if (currentMatch == findTeamID && (Number(currentRound) + 1 == findMatchRound)) {
+                            console.log('двигаем подедителя');
+                            findTeamName.textContent = setTeamName;
+                            findTeemm.classList.remove('not-ready')
+                            findTeemm.classList.add('ready')
+                        }
+                    });
+                }
+            });
+        }
 
+        function unblockMatchButton() {
+            allMatchArr.forEach(match => {
+                let teamArr = match.querySelectorAll('.tournament__team')
+                let battleButton = match.querySelector('.tournament__button-play-match')
+                let readyCounter = 0;
+                teamArr.forEach(team => {
+                    if (team.classList.contains('ready')) {
+                        readyCounter++
+                    }
+
+                    if (team.classList.contains('winner')) {
+                        console.log('есть победитель');
+                        battleButton.classList.add('hidden')
+                    }
+                });
+
+                if (readyCounter == 2) {
+                    battleButton.classList.remove('hidden')
+                }
+            });
+        }
+
+        function showChampion() {
+
+            teamArr.forEach(team => {
+                let mmatchRound = team.closest('.tournament__round')
+
+                if (team.classList.contains('winner') && mmatchRound.classList.contains('final')) {
+                    team.classList.add('champion')
+                }
+
+            });
+
+        }
 
         function startMatch() {
 
             if (teamArr[0].classList.contains('ready') && teamArr[1].classList.contains('ready')) {
                 console.log('команды готовы');
                 getScore()
+                mooveWinner()
+                unblockMatchButton()
 
-                // if () {
+                showChampion()
 
-
-                // }
             } else {
                 console.log('команды не готовы');
             }
