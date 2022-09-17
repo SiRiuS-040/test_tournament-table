5efc8bf8c338c0ac5e1b12fae1648bcaaa059e11
SiRiuS-040 <79222154+SiRiuS-040@users.noreply.github.com>
2022-09-17 11:34:58 +0300

    логотипы

--- js/script.js
+++ js/script.js
@@ -2,89 +2,87 @@
 let TEAM_DATA = [
     {
         id: "1",
-        name: "Участник 1",
+        name: "Team 1",
         logo: "https://fire-logo.ru/img/mascots.png"
     },
     {
         id: "2",
-        name: "Участник 2",
+        name: "Team 2",
         logo: "https://papik.pro/uploads/posts/2021-11/thumbs/1636129744_53-papik-pro-p-krutie-logotipi-foto-55.png"
     },
     {
         id: "3",
-        name: "Участник 3",
-        logo: "https://fire-logo.ru/img/mascots.png"
+        name: "Team 3",
+        logo: "https://i.imgur.com/CJyhIwX.png"
     },
     {
         id: "4",
-        name: "Участник 4",
-        logo: "https://papik.pro/uploads/posts/2021-11/thumbs/1636129744_53-papik-pro-p-krutie-logotipi-foto-55.png"
+        name: "Team 4",
+        logo: "https://papik.pro/uploads/posts/2022-01/thumbs/1643624583_41-papik-pro-p-logotip-cherep-41.png"
     },
     {
         id: "5",
-        name: "Участник 5",
+        name: "Team 1",
         logo: "https://fire-logo.ru/img/mascots.png"
     },
     {
         id: "6",
-        name: "Участник 6",
+        name: "Team 2",
         logo: "https://papik.pro/uploads/posts/2021-11/thumbs/1636129744_53-papik-pro-p-krutie-logotipi-foto-55.png"
     },
     {
         id: "7",
-        name: "Участник 7",
-        logo: "https://fire-logo.ru/img/mascots.png"
+        name: "Team 3",
+        logo: "https://i.imgur.com/CJyhIwX.png"
     },
     {
         id: "8",
-        name: "Участник 8",
-        logo: "https://papik.pro/uploads/posts/2021-11/thumbs/1636129744_53-papik-pro-p-krutie-logotipi-foto-55.png"
+        name: "Team 4",
+        logo: "https://papik.pro/uploads/posts/2022-01/thumbs/1643624583_41-papik-pro-p-logotip-cherep-41.png"
     },
     {
         id: "9",
-        name: "Участник 9",
+        name: "Team 1",
         logo: "https://fire-logo.ru/img/mascots.png"
     },
     {
         id: "10",
-        name: "Участник 11",
+        name: "Team 2",
         logo: "https://papik.pro/uploads/posts/2021-11/thumbs/1636129744_53-papik-pro-p-krutie-logotipi-foto-55.png"
     },
     {
         id: "11",
-        name: "Участник 11",
-        logo: "https://fire-logo.ru/img/mascots.png"
+        name: "Team 3",
+        logo: "https://i.imgur.com/CJyhIwX.png"
     },
     {
         id: "12",
-        name: "Участник 12",
-        logo: "https://papik.pro/uploads/posts/2021-11/thumbs/1636129744_53-papik-pro-p-krutie-logotipi-foto-55.png"
+        name: "Team 4",
+        logo: "https://papik.pro/uploads/posts/2022-01/thumbs/1643624583_41-papik-pro-p-logotip-cherep-41.png"
     },
     {
         id: "13",
-        name: "Участник 13",
+        name: "Team 1",
         logo: "https://fire-logo.ru/img/mascots.png"
     },
     {
         id: "14",
-        name: "Участник 15",
+        name: "Team 2",
         logo: "https://papik.pro/uploads/posts/2021-11/thumbs/1636129744_53-papik-pro-p-krutie-logotipi-foto-55.png"
     },
     {
         id: "15",
-        name: "Участник 15",
-        logo: "https://fire-logo.ru/img/mascots.png"
+        name: "Team 3",
+        logo: "https://i.imgur.com/CJyhIwX.png"
     },
     {
         id: "16",
-        name: "Участник 16",
-        logo: "https://papik.pro/uploads/posts/2021-11/thumbs/1636129744_53-papik-pro-p-krutie-logotipi-foto-55.png"
+        name: "Team 4",
+        logo: "https://papik.pro/uploads/posts/2022-01/thumbs/1643624583_41-papik-pro-p-logotip-cherep-41.png"
     },
 ]
-
-const MAX_GAME_SCORE = 10;
-
-const MAX_TEAM_NAME = 15;
+let MAX_GAME_SCORE = 10;
+let MAX_TEAM_NAME = 15;
 
 
 document.addEventListener('DOMContentLoaded', function (evt) {
